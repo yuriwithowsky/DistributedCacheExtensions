f7107a75e3e2a52821bfccf4cb5b4b8fe7704ece
Yuri Withowsky <yuri.jivago14@gmail.com>
2023-09-16 16:17:28 -0300

    Add project files.

---
+++ DistributedCacheExtensions/DistributedCacheSerializerExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+namespace DistributedCache.Extensions;
+
+public static class DistributedCacheSerializerExtensions
+{
+    public static T? Get<T>(this IDistributedCache cache, string key) where T : class
+    {
+        var value = cache.Get(key);
+        return value is null ? null : JsonSerializer.Deserialize<T>(value);
+    }
+    public static async ValueTask<T?> GetAsync<T>(this IDistributedCache cache, string key) where T : class
+    {
+        var value = await cache.GetAsync(key).ConfigureAwait(false);
+        return value is null ? null : JsonSerializer.Deserialize<T>(value);
+    }
+
+    public static void Set<T>(this IDistributedCache cache, string key, T value) where T : class
+    {
+        var serialized = JsonSerializer.Serialize(value);
+
+        cache.SetString(key, serialized);
+    }
+
+    public static async Task SetAsync<T>(this IDistributedCache cache, string key, T value) where T : class
+    {
+        var serialized = JsonSerializer.Serialize(value);
+
+        await cache.SetStringAsync(key, serialized).ConfigureAwait(false);
+    }
+
+    public static T? Get<T>(IDistributedCache cache, string key, Func<T> getMethod) where T : class
+    {
+        var value = cache.Get<T>(key);
+        if (value is null)
+        {
+            value = getMethod();
+
+            if (value is not null)
+            {
+                cache.Set(key, value);
+            }
+        }
+        return value;
+    }
+
+    public static async Task<T?> GetAsync<T>(IDistributedCache cache, string key, Func<Task<T>> getMethod) where T : class
+    {
+        var value = await cache.GetAsync<T>(key).ConfigureAwait(false);
+
+        if (value is null)
+        {
+            value = await getMethod().ConfigureAwait(false);
+
+            if (value is not null)
+            {
+                await cache.SetAsync(key, value).ConfigureAwait(false);
+            }
+        }
+        return value;
+    }
+}
---
+++ DistributedCacheExtensionsTests/FakeDistributedCache.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+
+namespace DistributedCache.Extensions.Tests;
+public class FakeDistributedCache : IDistributedCache
+{
+    private Dictionary<string, byte[]> _cache = new Dictionary<string, byte[]>();
+
+    public byte[]? Get(string key) =>
+        _cache.GetValueOrDefault(key);
+
+    public Task<byte[]?> GetAsync(string key, CancellationToken token = default) =>
+        Task.FromResult(_cache.GetValueOrDefault(key));
+
+    public void Refresh(string key)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task RefreshAsync(string key, CancellationToken token = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Remove(string key) =>
+        _cache.Remove(key);
+
+    public Task RemoveAsync(string key, CancellationToken token = default) =>
+        Task.FromResult(_cache.Remove(key));
+
+    public void Set(string key, byte[] value, DistributedCacheEntryOptions options) =>
+        _cache.Add(key, value);
+
+    public Task SetAsync(string key, byte[] value, DistributedCacheEntryOptions options, CancellationToken token = default)
+    {
+        _cache.Add(key, value);
+        return Task.CompletedTask;
+    }
+}
